48b4786188cb95ef8d6b6c42d5d093b5ce7585a4
alexsmaldone <alexsmaldone@gmail.com>
2022-11-26 23:03:12 -0500

    largest factor

--- 02-Functions.py
+++ 02-Functions.py
@@ -40,3 +40,20 @@ def two_of_three(i, j, k):
     50
     """
     return min(i*i + j*j, j*j + k*k, i*i + k*k )
+
+
+def largest_factor(n):
+    """Return the largest factor of n that is smaller than n.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factor is 1 since 13 is prime
+    1
+    """
+    for num in range(n-1, 0, -1):
+      if n % num == 0:
+        return num
+
+print(largest_factor(13))
