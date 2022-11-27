0550b54e282f1242b2c7d8d9f5ec0b66adb09392
alexsmaldone <alexsmaldone@gmail.com>
2022-11-27 15:54:36 -0500

    controlflow lab

---
+++ 03-Functions-Control.py
@@ -0,0 +1,71 @@
+# SKIP bc v easy
+
+
+
+# >>> def xk(c, d):
+
+
+
+# ...     if c == 4:
+# ...         return 6
+# ...     elif d >= 4:
+# ...         return 6 + 7 + c
+# ...     else:
+# ...         return 25
+# >>> xk(10, 10)
+# ______
+
+# >>> xk(10, 6)
+# ______
+
+# >>> xk(4, 6)
+# ______
+
+# >>> xk(0, 0)
+# ______
+
+
+# >>> def how_big(x):
+# ...     if x > 10:
+# ...         print('huge')
+# ...     elif x > 5:
+# ...         return 'big'
+# ...     elif x > 0:
+# ...         print('small')
+# ...     else:
+# ...         print("nothing")
+# >>> how_big(7)
+# ______
+
+# >>> how_big(12)
+# ______
+
+# >>> how_big(1)
+# ______
+
+# >>> how_big(-1)
+# ______
+
+
+>>> n = 3
+>>> while n >= 0:
+...     n -= 1
+...     print(n)
+______
+
+
+>>> positive = 28
+>>> while positive:
+...    print("positive?")
+...    positive -= 3
+______
+
+
+>>> positive = -9
+>>> negative = -12
+>>> while negative:
+...    if positive:
+...        print(negative)
+...    positive += 3
+...    negative += 3
+______
