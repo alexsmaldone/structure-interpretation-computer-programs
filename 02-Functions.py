46a9c3407f813597d12a7043377f1ad2ff591044
alexsmaldone <alexsmaldone@gmail.com>
2022-11-26 23:03:33 -0500

    added hailstone problem

--- 02-Functions.py
+++ 02-Functions.py
@@ -56,4 +56,24 @@ def largest_factor(n):
       if n % num == 0:
         return num
 
-print(largest_factor(13))
+
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> b = hailstone(1)
+    1
+    >>> b
+    1
+    """
+    "*** YOUR CODE HERE ***"
