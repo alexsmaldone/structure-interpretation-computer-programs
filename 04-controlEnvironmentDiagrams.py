099e5f22bee92a1b30823bb978d96c2c9af53277
alexsmaldone <alexsmaldone@gmail.com>
2022-11-27 22:49:03 -0500

    jacket weather

--- 04-controlEnvironmentDiagrams.py
+++ 04-controlEnvironmentDiagrams.py
@@ -8,4 +8,34 @@ def special_case():
         x += 4
     return x
 
-print(special_case())
+def just_in_case():
+    x = 10
+    if x > 0:
+        x += 2
+    if x < 13:
+        x += 3
+    if x % 2 == 1:
+        x += 4
+    return x
+
+def case_in_point():
+    x = 10
+    if x > 0:
+        return x + 2
+    if x < 13:
+        return x + 3
+    if x % 2 == 1:
+        return x + 4
+    return x
+
+
+def wears_jacket_with_if(temp, raining):
+    """
+    >>> wears_jacket_with_if(90, False)
+    False
+    >>> wears_jacket_with_if(40, False)
+    True
+    >>> wears_jacket_with_if(100, True)
+    True
+    """
+    return temp < 60 or raining
